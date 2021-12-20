be5a3f5926ea54cee97b88966229518b49964e9a
Андрей Беляков <ttxiom@gmail.com>
2021-12-20 21:04:41 +0500

    up

---
+++ theme-05-expsist/part-2-frontend/01_es_bin_js/es_bin.js
@@ -0,0 +1,38 @@
+﻿var post; // текущий пост
+var txtQuest; // поле для вывода вопроса
+var txtYes; // поле варианта ответа
+var txtNo; // поле варианта ответа
+var colors; // массив цветов выкл/вкл
+var run; // запущено или остановлено приложение
+
+function init() {
+    run = true; // запустить приложение
+    colors = ['#464', '#777']; // назначить цвета ВКЛ/ВЫКЛ
+    txtQuest = document.getElementById("quest");
+    txtYes = document.getElementById("itemYes");
+    txtNo = document.getElementById("itemNo");
+    txtTitle = document.getElementById("title");
+    txtTitle.innerHTML = es.title;
+    post = Object.keys(es.dict)[0]; // узнать вопрос
+    txtQuest.innerHTML = post; // опубликовать вопрос
+}
+
+function next(answer) {
+    if (run) {
+        post = Object.values(es.dict[post])[answer-1];
+        txtQuest.innerHTML = post;
+        if (post.slice(-1) == '.') {
+            run = false; // остановить работу ЭС
+            document.getElementById('img').src = "img/" + post + "png";
+        }    
+    }
+}
+
+function itemOn(item, num) {
+    if (item=='Yes') {
+        txtYes.style = 'background-color: ' + colors[num];
+    }
+    if (item=='No') {
+        txtNo.style = 'background-color: ' + colors[num];
+    }
+}
---
+++ theme-05-expsist/part-2-frontend/02_es_bin_jq/es_bin.js
@@ -0,0 +1,43 @@
+﻿// при подключенной библиотеке jQuery в самом конце html
+var post; // текущий пост
+var colors; // массив цветов выкл/вкл
+var run; // запущено или остановлено приложение
+
+$(function() {
+    colors = ['#464', '#777'];
+    $('#title').html(es.title);
+    // next("Yes");
+});
+
+$('img').on('click', function() {
+    location.reload();
+});
+
+function next(item) {
+    posLines++;
+    posLevel++;
+    if (item=='No') findPosLine();
+    var result = lines[posLines].substring(posLevel);
+    if (result[0]==charEnd) {
+        result = result.substring(1);
+        $('img').attr("src", "img/" + result + ".png");
+    }
+    else
+        result = result + '?';
+    $('.quest').html(result);
+}
+
+$('.item').mousedown(function() {
+    var $id = $(this).attr('id'); // узнаём id элемента
+    next($id);
+});
+
+$('.item').mouseover(function() {
+    var $id = $(this).attr('id'); // узнаём id элемента
+    $('#'+$id).css('background-color', colors[1])
+});
+
+$('.item').mouseout(function() {
+    var $id = $(this).attr('id'); // узнаём id элемента
+    $('#'+$id).css('background-color', colors[0])
+});
