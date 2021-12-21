967b3c74ca829ffeaec084f33e7a9f877b7705c7
Андрей Беляков <ttxiom@gmail.com>
2021-12-21 13:34:24 +0500

    up

--- theme-05-expsist/part-2-frontend/02_es_bin_jq/es_bin.js
+++ theme-05-expsist/part-2-frontend/02_es_bin_jq/es_bin.js
@@ -1,43 +1,56 @@
-﻿// при подключенной библиотеке jQuery в самом конце html
-var post; // текущий пост
+﻿// при подключенной библиотеке jQuery в самом конце html-страницы
+// глобальные переменные
+var post; // текущий пост (не quest, так как там в конце будет ответ)
 var colors; // массив цветов выкл/вкл
 var run; // запущено или остановлено приложение
 
-$(function() {
-    colors = ['#464', '#777'];
-    $('#title').html(es.title);
-    // next("Yes");
-});
-
-$('img').on('click', function() {
-    location.reload();
-});
-
-function next(item) {
-    posLines++;
-    posLevel++;
-    if (item=='No') findPosLine();
-    var result = lines[posLines].substring(posLevel);
-    if (result[0]==charEnd) {
-        result = result.substring(1);
-        $('img').attr("src", "img/" + result + ".png");
+/**
+ * функция для поиска поста
+ * @param {ответ-Yes-No} answer 
+ */ 
+let next = function (answer) {
+    let index = answer == "Yes"? 0: 1;
+    if (run) {
+        post = Object.values(es.dict[post])[index];
+        $('#quest').html(post); // публикуем пост (вопрос/ответ)
+        add_post(post);
+        if (post.slice(-1) == '.') { // если уже ответ
+            run = false; // остановить работу ЭС
+            $('img').attr("src", "img/" + post + "png");
+        }
     }
-    else
-        result = result + '?';
-    $('.quest').html(result);
 }
+
+let add_post = function (post) {
+    let text = $('#questions').html();
+    $('#questions').html(text + '\n' + post);
+} 
+
+// обработчики событий jQuery
+
+$(function() {
+    run = true; // запускаем приложение
+    colors = ['#464', '#777'];
+    $('#title').html(es.title); // для объекта в DOM`е с таким id
+    post = Object.keys(es.dict)[0]; // узнаем первый вопрос
+    $('#quest').html(post);
+    $('#questions').html("Все промежуточные вопросы: ");
+    add_post(post);
+}); // эта анонимная функция запускается при загрузки страницы
+
+$('img').on('click', () => location.reload()); // для всех такого тега img
 
 $('.item').mousedown(function() {
     var $id = $(this).attr('id'); // узнаём id элемента
     next($id);
 });
 
-$('.item').mouseover(function() {
+$('.item').mouseover(function() { // для всех такого класса item
     var $id = $(this).attr('id'); // узнаём id элемента
-    $('#'+$id).css('background-color', colors[1])
+    $('#'+$id).css('background-color', colors[1]);
 });
 
 $('.item').mouseout(function() {
     var $id = $(this).attr('id'); // узнаём id элемента
-    $('#'+$id).css('background-color', colors[0])
+    $('#'+$id).css('background-color', colors[0]);
 });
