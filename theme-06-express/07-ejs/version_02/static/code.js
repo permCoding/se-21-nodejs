925aa314173fecad4647026b396ae61478c6aa29
Андрей Беляков <ttxiom@gmail.com>
2022-01-25 22:49:00 +0500

    up

---
+++ theme-06-express/07-ejs/version_02/static/code.js
@@ -0,0 +1,10 @@
+﻿// service variables
+let colors = ['#464', '#777']; // массив цветов выкл/вкл
+
+// event handlers
+document // при клике по ячейке таблицы
+    .querySelectorAll('.dialog .answer') // найти массив ячеек таблицы с ответами
+    .forEach(td_answer => { // для каждой ячейки назначить обработчики событий
+        td_answer.addEventListener('mouseenter', () => td_answer.style.backgroundColor = colors[1]);
+        td_answer.addEventListener('mouseleave', () => td_answer.style.backgroundColor = colors[0]);
+    });
