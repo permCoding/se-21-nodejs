232a8036898b2f1d30bca6f63eba59fdad1d249a
Андрей Беляков <ttxiom@gmail.com>
2022-02-18 10:19:12 +0500

    pdf_files

--- theme-99-service/RSoF for browser/public/docs/07.js
+++
@@ -1,5 +0,0 @@
-const { get_list } = require("./tools");
-
-let dir_list = get_list(__dirname);
-
-dir_list.forEach(line => console.log(line));
--- theme-99-service/RSoF for browser/public/utils.js
+++
@@ -1,6 +0,0 @@
-module.exports.server_info = function (params) {
-    console.log(`Сервер запущен - http://${params.hostname}:${params.port}/`);
-    let hotKeys = "Ctrl+C";
-    console.log(`остановить сервер - ${hotKeys}`);
-};
-
