189143ca22731bb0bc5c3fe05fcdabbe94053a24
permCoding <ttxiom@gmail.com>
2022-04-12 15:53:24 +0500

    flatten array

---
+++ part-1/themes/theme-02-array/03-flat/01.js
@@ -0,0 +1,25 @@
+let get_flat_arr = (array) => {
+    let flat_array = []
+    for (let i = 0; i < array.length; i++) {
+        if (Array.isArray(array[i])) {
+            let current_array = get_flat_arr(array[i])
+            for (let j = 0; j < current_array.length; j++) {
+                flat_array.push(current_array[j])
+            }
+        }
+        else {
+            flat_array.push(array[i])
+        }
+    }
+    return flat_array
+}
+
+let arr = [
+    0,
+    [],
+    666,
+    [[[9]], 111],
+    333
+]
+
+console.table(get_flat_arr(arr))
---
+++ part-1/themes/theme-02-array/03-flat/02.js
@@ -0,0 +1,24 @@
+let get_flat_arr = (array) => {
+    let flat_array = []
+    for (let item of array) {
+        if (Array.isArray(item)) {
+            flat_array = [...flat_array, ...get_flat_arr(item)]
+        }
+        else {
+            flat_array.push(item)
+        }
+    }
+    return flat_array
+}
+
+let arr = [
+    0,
+    [10],
+    666,
+    [[[9]], 111],
+    333
+]
+
+let flat_arr = get_flat_arr(arr).sort((a, b) => a - b)
+
+console.table(flat_arr)
