80310e828f9601f89a449fdbf51aa068131df14a
Андрей Беляков <ttxiom@gmail.com>
2022-05-16 15:11:05 +0500

    up

--- part-2/themes/08-list_files/01-browser/app-03ver.js
+++ part-2/themes/08-list_files/01-browser/app-03ver.js
@@ -9,42 +9,50 @@ app.use('/public', express.static('public'));
 app.use('/css', express.static('css'));
 app.set('view engine', 'ejs'); // npm i ejs
 const params = {"port": 3000, "hostname": "127.0.0.1"};
-const dir_files = path.join("public", "docs");
+// const dir_files = path.join("public", "docs");
+const dir_files = "docs";
 
 // паттерн проектирования MVC
 
 // model data
 const model_data = require("./models/model").model_data;
-const get_list = require("./models/model").get_list_files;
+// const get_list = require("./models/model").get_list_files;
+const get_list = require("./models/model").get_list_files_full;
 
 // controller
 app.get('/', (req, res) => {
-    model_data.list_files = get_list(dir_files);
+    model_data.list_files = get_list(path.join("public", dir_files));
     res.render('index-03', model_data);
 });
 
-app.get('/docs/:id', (req, res) => {
+app.get(`/${dir_files}/:id`, (req, res) => {
     const id = Number(req.params.id);
     const file_name = model_data.list_files[id].file_name;
 
+    console.log(file_name);
+    let rel_path = path.join("public", dir_files, file_name);
+    let file_path = path.join(__dirname, rel_path);
+
     // -- 0 -- отправить в браузер клиенту строку - адрес файла
-    // res.send(file_name);
+    // res.send(file_path);
 
     // -- 1 -- render - отрендерить в html-странице
-    let content = readFileSync(path.join(dir_files, file_name), 'utf-8');
-    res.render('file', { content: content, title: file_name });
+    // let content = readFileSync(file_path, 'utf-8');
+    // res.render('file', { content: content, title: file_name });
 
     // -- 2 -- send - отправить на загрузку клиенту
-    // console.log("send file completed - " + file_name);
-    // res.sendFile(path.join(__dirname, dir_files, file_name));
+    // res.sendFile(file_path);
 
     // -- 3 -- download - отправить на загрузку клиенту
-    // console.log("download completed - " + file_name);
-    // res.download(path.join(__dirname, dir_files, file_name));
+    // res.download(file_path);
+    // res.download(file_path, file_name, (err) => {
+    //     if (err) console.error(err);
+    //     else console.log("successful download - " + file_name);
+    // });
     
     // -- 4-- stream
     // const {createReadStream} = require("fs");
-    // let file_stream = createReadStream(path.join(__dirname, dir_files, file_name));
+    // let file_stream = createReadStream(file_path);
     // file_stream.pipe(res);
     // file_stream.on("finish", () => {
     //     file_stream.close();
@@ -59,14 +67,3 @@ app.listen(params.port, params.hostname, () => {
     console.log(`Сервер запущен - http://${params.hostname}:${params.port}/`);
     console.log('остановить сервер - Ctrl+C');
 });
-
-
-    // это черновики
-
-    // let rel_path = path.join("public", dir_files, file_name);
-    // let file_path = path.join(__dirname, rel_path);
-
-    // res.download(file_path, file_name, (err) => {
-    //     if (err) console.error(err);
-    //     else console.log("successful download - " + file_name);
-    // });
