50d7b84d385b7909f6dacc1c89515a1fbd9be6c8
Андрей Беляков <ttxiom@gmail.com>
2022-07-22 23:46:08 +0500

    crypto

--- part-2/themes/10-service/crypto/05.js
+++ part-2/themes/10-service/crypto/05.js
@@ -1,46 +1,31 @@
 const crypto = require('crypto');
+const fs = require('fs');
 
-const start = Date.now();
+// https://www.geeksforgeeks.org/node-js-crypto-pbkdf2-method/
+// crypto.pbkdf2(password, salt, iterations, keylen, digest, callback)
 
-// node.js работает под капотом в 4 потока для async функций
-// но преимущество не в 4 раза, а в 2 - время уходит на организацию и переключение
-for (let iter = 0; iter < 12; iter++) {
-    // сравним вариант асинхронный
-    // crypto.pbkdf2("admin", '5', 1000000, 64, "sha512", () => {
-    //     console.log(`iter = ${iter};\t${Date.now()-start}ms`)
-    // });
-    // и синхронный
-    let hash = crypto.pbkdf2Sync("admin", '5', 1000000, 64, "sha512");
-    console.log(`iter = ${iter};\t${Date.now()-start}ms`)
+let arr = [
+    'qwerty',
+    '123456',
+    '0000',
+    'test',
+    'password'
+];
+
+let hashes = [];
+for (let item of arr) {
+    let hash = crypto.pbkdf2Sync(item, 'salt', 1000, 32, "sha512");
+    let h = { 
+        'item': item, 
+        'hash': hash.toString('hex') 
+    };
+    hashes.push(h);
 }
 
-/*
-пример асинхронного вывода
-iter = 3;       1502ms
-iter = 1;       1506ms
-iter = 0;       1561ms
-iter = 2;       1561ms
-iter = 4;       3018ms
-iter = 7;       3024ms
-iter = 5;       3046ms
-iter = 6;       3065ms
-iter = 8;       4471ms
-iter = 9;       4523ms
-iter = 11;      4543ms
-iter = 10;      4560ms
-пример синхронного вывода
-iter = 0;       734ms
-iter = 1;       1476ms
-iter = 2;       2226ms
-iter = 3;       2960ms
-iter = 4;       3696ms
-iter = 5;       4435ms
-iter = 6;       5171ms
-iter = 7;       5913ms
-iter = 8;       6649ms
-iter = 9;       7389ms
-iter = 10;      8160ms
-iter = 11;      8945ms
-*/
+console.table(hashes);
 
-
+const jsonContent = JSON.stringify(hashes, null, 4); // https://learn.javascript.ru/json
+fs.writeFile("./hashes.json", jsonContent, 'utf8', err => {
+    if (err) throw err;
+    console.log("The file with hashes was saved.");
+});
